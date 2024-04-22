c94f117c10d0d975b07a864b52de078c685b2259
PepoTH <cesardarizaleta@gmail.com>
2024-04-22 12:02:35 -0400

    gestor simple

--- gestor.py
+++ gestor.py
@@ -7,8 +7,13 @@ def main(page: ft.Page):
     page.window_height = 400
     page.title = "Gestor de Archivos"
 
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
     page.theme_mode = ft.ThemeMode.LIGHT
 
-    page.add(ft.FilePicker())
+    picker = ft.FilePicker()
+    btn = ft.ElevatedButton("Escoger",on_click=picker.pick_files)
+    
+    page.add(ft.Row([btn,picker],alignment=ft.MainAxisAlignment.CENTER))
     
 ft.app(target=main)
