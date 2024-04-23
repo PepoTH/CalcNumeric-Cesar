e231a2c6b2c4e9b81a4b159c99f4e869bd73b19a
PepoTH <cesardarizaleta@gmail.com>
2024-04-23 00:03:04 -0400

    Binario

--- binario.py
+++ binario.py
@@ -12,10 +12,29 @@ def main(page: ft.Page):
     
     page.window_center()
 
+    def decimal(e):
+        if(txt.value != ""):
+            result.value = str(int(txt.value,2))
+            result.update()
+
+    def binario(e):
+        if(txt.value != ""):
+            n = int(txt.value)
+            lista = []
+            while(n > 0):
+                lista.append(str(n % 2))
+                n = n // 2
+
+            string = lista[::-1]
+            result.value = ''.join(string)
+            result.update()
+
     txt = ft.TextField(label="Binario/Decimal")
+    result = ft.TextField(disabled=True)
 
     page.add(ft.Container(ft.Column([
-        txt
+        txt,ft.ElevatedButton("Binario-Decimal",on_click=decimal),
+        ft.ElevatedButton("Decimal-Binario",on_click=binario),result
     ])))
     
 ft.app(target=main)
