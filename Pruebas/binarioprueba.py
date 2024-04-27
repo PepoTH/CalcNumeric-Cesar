fefe346ae1a4b96192c5296d36d1e8dd3ffe268f
PepoTH <cesardarizaleta@gmail.com>
2024-04-27 15:24:53 -0400

    Agregando eventos

--- Pruebas/binarioprueba.py
+++ Pruebas/binarioprueba.py
@@ -11,5 +11,26 @@ def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     
     page.window_center()
+
+    page.theme_mode = ft.ThemeMode.LIGHT
+
+    def accion(e):
+        if(txt.value != ""):
+            texto = ["Hola"]
+            texto.remove("Hola")
+            
+            print(texto)
+
+    def cambio(e):
+        if(txt.value != ""):
+            if not(txt.value[len(txt.value) - 1].isdecimal()):
+                txt.value = txt.value.replace(txt.value[-1],"")
+                txt.update()
+            
+            
+
+    txt = ft.TextField(text_align="right",on_change=cambio)
+
+    page.add(txt,ft.ElevatedButton("Convertir",on_click=accion))
     
 ft.app(target=main)
