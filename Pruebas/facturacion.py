0f7102b44d1b678a75ea3f3c426206808c88fc53
PepoTH <cesardarizaleta@gmail.com>
2024-04-29 20:20:04 -0400

    Ubicando tabla

--- Pruebas/facturacion.py
+++ Pruebas/facturacion.py
@@ -5,17 +5,14 @@ def main(page: ft.Page):
     page.window_width = 400
     page.window_height = 800
     page.padding = 40
-    
 
     page.theme_mode = ft.ThemeMode.LIGHT
     
     page.window_center()
 
-    
-
     container = ft.Container(ft.Column([
         ft.Text("Prueba")
-    ]))
+    ]),bgcolor="red",width=page.window_width*0.80,height=page.window_height*0.7)
 
     page.add(ft.Text("Facturas",weight=ft.FontWeight.BOLD,size=30),container)
     
