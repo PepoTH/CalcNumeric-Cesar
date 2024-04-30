e7f4b3a263fa37acb4b4319c7c79b7e658494edf
PepoTH <cesardarizaleta@gmail.com>
2024-04-30 18:42:36 -0400

    Probando Numpy

--- Pruebas/Numpy/numpyPrueba.py
+++ Pruebas/Numpy/numpyPrueba.py
@@ -4,4 +4,7 @@ a = np.arange(6)
 a2 = a[np.newaxis, :]
 a2.shape
 
+#Creacion de Vector
+a = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+
 print(a[0])
---
+++ Pruebas/Numpy/vector.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+#Creacion Vector
+vector = np.array([1,2,3,4])
+
+#Matriz
+
+matriz = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+#vectores con espacios
+# (Donde inicia, Donde termina, cantida de elementos en el vector)
+lin = np.linspace(0, 11, num=12)
+
+print(lin)
