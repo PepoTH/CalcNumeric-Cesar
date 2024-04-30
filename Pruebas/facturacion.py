5ba83b4d37746738abee26b10ff56f15a850b62d
PepoTH <cesardarizaleta@gmail.com>
2024-04-29 20:17:12 -0400

    iniciando tabla

--- Pruebas/facturacion.py
+++ Pruebas/facturacion.py
@@ -4,19 +4,19 @@ def main(page: ft.Page):
     page.window_resizable = False
     page.window_width = 400
     page.window_height = 800
-    page.padding = 30
+    page.padding = 40
     
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     page.theme_mode = ft.ThemeMode.LIGHT
     
     page.window_center()
 
+    
+
     container = ft.Container(ft.Column([
         ft.Text("Prueba")
     ]))
 
-    page.add(container)
+    page.add(ft.Text("Facturas",weight=ft.FontWeight.BOLD,size=30),container)
     
 ft.app(target=main)
