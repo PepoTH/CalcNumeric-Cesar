c915821617bc1df639f2980cf0e8699cd401a675
PepoTH <cesardarizaleta@gmail.com>
2024-05-06 14:01:01 -0400

    Creando Coordenadas

---
+++ Pruebas/Flet/coords.py
@@ -0,0 +1,20 @@
+import flet as ft
+
+def main(page: ft.Page):
+
+    page.window_resizable = False
+
+    txt = ft.Text('Hi')
+
+    #Solo se pueden controlar valores de coordenadas con stack y poniendole top y left siendo
+    #x e y, top el espacio hacia arriba del componente y left el espacio hacia la izquierda
+
+    stack = ft.Stack([
+        ft.Column([
+            txt
+        ],top=60,left=30)
+    ])
+    
+    page.add(stack)
+    
+ft.app(target=main)
