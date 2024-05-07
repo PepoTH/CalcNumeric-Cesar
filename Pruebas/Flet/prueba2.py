bb927c81ee7d7ad387175a29358bbaafdf90a8ee
PepoTH <cesardarizaleta@gmail.com>
2024-05-06 22:45:35 -0400

    Agregando iphone

--- Pruebas/Flet/prueba2.py
+++ Pruebas/Flet/prueba2.py
@@ -1,16 +1,20 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.window_resizable = False
-    page.window_width = 300
-    page.window_height = 300
-    page.padding = 30
-    
-    
-    page.window_center()
+    page.add(
+        ft.Container(
+            width=200,
+            height=200,
+            bgcolor=ft.colors.GREEN_200,
+            border_radius=10,
+            content=ft.Container(
+                width=100,
+                height=100,
+                bgcolor=ft.colors.BLUE_200,
+                border_radius=5,
+                alignment=ft.alignment.center,  # center the second container inside the first container
+            ),
+        )
+    )
 
-    txt = ft.TextField()
-
-    page.add(txt,ft.top)
-    
 ft.app(target=main)
