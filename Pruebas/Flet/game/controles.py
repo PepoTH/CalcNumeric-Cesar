32894630e93d3f9da01db2246d9ba165856939db
PepoTH <cesardarizaleta@gmail.com>
2024-05-19 19:32:37 -0400

    Agregando segundo jugador

--- Pruebas/Flet/game/controles.py
+++ Pruebas/Flet/game/controles.py
@@ -12,27 +12,41 @@ def main(page: ft.Page):
 
     def move(e):
         if(e.key == 'Arrow Left' and jugador.x >= 10):
-                jugador.x -= 10
+                jugador.x -= 20
         elif(e.key == 'Arrow Right' and jugador.x <= page.window_width-40):
-            jugador.x += 10
+            jugador.x += 20
         elif(e.key == 'Arrow Up' and jugador.y >= 10):
-            jugador.y -= 10
+            jugador.y -= 20
         elif(e.key == 'Arrow Down' and jugador.y <= page.window_height-80):
-            jugador.y += 10
+            jugador.y += 20
+
+        if(e.key == 'A' and jugador2.x >= 10):
+                jugador2.x -= 20
+        elif(e.key == 'D' and jugador2.x <= page.window_width-40):
+            jugador2.x += 20
+        elif(e.key == 'W' and jugador2.y >= 10):
+            jugador2.y -= 20
+        elif(e.key == 'S' and jugador2.y <= page.window_height-80):
+            jugador2.y += 20
 
         page.update()
 
-    jugador = ft.canvas.Rect(0,page.window_height-70,30,50)
+    jugador = ft.canvas.Rect(10,page.window_height-100,30,50,border_radius=30,
+                             paint=ft.Paint(color='blue'))
+    jugador2 = ft.canvas.Rect(page.window_width-40,page.window_height-100,30
+                              ,50,border_radius=30,paint=ft.Paint(color='red'))
     obj = ft.canvas.Circle(10,10,10)
 
     cv = ft.canvas.Canvas([
-        jugador,obj
+        jugador,obj,jugador2
     ])
 
     page.on_keyboard_event = move
 
     page.add(cv)
 
+    #Configuracion de distancia recorrida
+    distancia = 5
     modeH = True
     modeV = True
 
@@ -40,29 +54,33 @@ def main(page: ft.Page):
         #Configuracion Horizontal
         if(modeH):
             if(obj.x >= 0 and obj.x <= page.window_width-20):
-                obj.x += 10
+                obj.x += distancia
             else:
                 modeH = False
         else:
             if(obj.x == 0):
                 modeH = True
             else:
-                obj.x -= 10
+                obj.x -= distancia
 
         if(modeV):
             if(obj.y >= 0 and obj.y <= page.window_height-35):
-                obj.y += 10
+                obj.y += distancia
             else:
                 modeV = False
         else:
             if(obj.y == 0):
                 modeV = True
             else:
-                obj.y -= 10
+                obj.y -= distancia
         
-        t.sleep(0.03)
+        t.sleep(0.01)
 
         if((obj.x >= jugador.x and obj.x <= jugador.x + jugador.width)and(obj.y >= jugador.y and obj.y <= jugador.y + jugador.height)):
+            modeV = not modeV
+            modeH = not modeH
+
+        if((obj.x >= jugador2.x and obj.x <= jugador2.x + jugador2.width)and(obj.y >= jugador2.y and obj.y <= jugador2.y + jugador2.height)):
             modeV = not modeV
             modeH = not modeH
         page.update()
