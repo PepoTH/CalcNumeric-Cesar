079fd5d4e22c1b929d99a307d7535c0631df99d6
PepoTH <cesardarizaleta@gmail.com>
2024-05-18 22:52:09 -0400

    Integrando jugador con balon

--- Pruebas/Flet/game/controles.py
+++ Pruebas/Flet/game/controles.py
@@ -1,6 +1,8 @@
 import flet as ft
+import time as t
 
 def main(page: ft.Page):
+    print('Inicio')
     page.window_resizable = False
     page.window_width = 500
     page.window_height = 300
@@ -21,13 +23,40 @@ def main(page: ft.Page):
         page.update()
 
     jugador = ft.canvas.Rect(page.window_width/2,page.window_height-70,30,50)
+    obj = ft.canvas.Circle(10,10,10)
 
     cv = ft.canvas.Canvas([
-        jugador
+        jugador,obj
     ])
 
     page.on_keyboard_event = move
 
     page.add(cv)
+
+    while(True):
+        if not(obj.x >= jugador.x and obj.x <= jugador.x + jugador.width and obj.y >= jugador.y and obj.y <= jugador.y + jugador.height):
+            if(obj.x == page.window_width - 30):
+                mode = False
+            if(obj.x == 10):
+                mode = True
+
+            if(mode):
+                obj.x += 10
+            else:
+                obj.x -= 10
+
+            if(obj.y == page.window_height - 40):
+                vertical = False
+            if(obj.y == 10):
+                vertical = True
+
+            if(vertical):
+                obj.y += 10
+            else:
+                obj.y -= 10
+            t.sleep(0.03)
+        
+        
+        page.update()
     
 ft.app(target=main)
